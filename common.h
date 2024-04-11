f56f82c7e86d29f95789f0acff50f654f5fb5c94
nanannnnnn <crimson.2021.iwasaki.tomoya@gmail.com>
2024-04-12 00:42:54 +0900

    feat: btree by c added

---
+++ common.h
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<ctype.h>
+#include<stdbool.h>
+#include<stdint.h>
+#include<float.h>
+#include<math.h>
+#include<string.h>
+#include<assert.h>
+#include<time.h>
+#include<sys/time.h>
+
+#define MAX(x, y) (x) > (y) ? (x) : (y)
+#define MIN(x, y) (x) < (y) ? (x) : (y)
